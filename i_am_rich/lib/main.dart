30071ea8deedf69ec02d541752d8bdf831eb703b
vishwaprabhupatil <vishwaprabhupatil@gmail.com>
2026-04-18 21:46:37 +0530

    18 april 2026

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -12,10 +12,7 @@ void main() {
         ),
         backgroundColor: Colors.blueGrey,
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTUhHf08INfuppoBCxtw80mlLHeI8HznguD4ct3FnAt8wNkAD0_USmxcbEjLG7Zh8vO8HGWVQwtOVGV858'),
-          ),
+          child: Image(image: AssetImage('images/diamond.png')),
         ),
       ),
     ),
